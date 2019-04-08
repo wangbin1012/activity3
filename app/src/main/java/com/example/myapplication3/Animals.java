8dad0bc9a244e2a1879377e522ba1e1a253be863
wangbin1012 <1138407154@qq.com>
2019-04-08 22:26:56 +0800

    安卓实验3

---
+++ app/src/main/java/com/example/myapplication3/Animals.java
@@ -0,0 +1,19 @@
+package com.example.myapplication3;
+
+public class Animals {
+    private String name;
+    private int imagesId;
+
+    public Animals(String name,int Id){
+        this.name = name;
+        this.imagesId = Id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImagesId(){
+        return imagesId;
+    }
+}
